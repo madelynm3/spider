4f0654560628d9c0c63e5438af5e747d02aabe42
Maddy McGreer <79669704+madelynm3@users.noreply.github.com>
2024-10-07 18:10:48 -0600

    practice

--- scraping.py
+++ scraping.py
@@ -1,34 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
-url = "https://www.rogerebert.com/reviews/megalopolis"
+url = "https://www.imdb.com/chart/top/"
 
 result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
+page = BeautifulSoup(result.content, "html.parser")
+scraped_movies = page.find_all('td', class_='titleColumn')
 
-# Find the rating elements
-# Inspect the page to find the correct tag and class name
-ratings = doc.find_all("span", class_="star-box")
-
-for rating in ratings:
-    print(rating.text.strip())
-
-
-# with open("index.html", "r") as f:
-#     doc = BeautifulSoup(f, "html.parser")
-
-# print(doc.prettify())
-
-# Locate tags
-# tag = doc.title
-# print(tag)
-
-# Access string within tag
-# tag = doc.title
-# tag.string = "hello"
-# print(tag)
-
-# Locate tags
-# tags = doc.find_all("p")[0]
-# print(tags.find_all("b"))
-
+movies = []
+for movie in scraped_movies:
+    title_element = movie.find('h3')
+    if title_element:
+        movies.append(title_element.get_text().strip())
+print(movies)
